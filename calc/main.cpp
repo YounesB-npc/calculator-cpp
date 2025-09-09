98b3665e4bb3c4259b62986810d2d8ffe8f4e2db
Younes <younes.bakir@mcvts.org>
2025-09-09 08:56:47 -0400

    Update main.cpp

--- calc/main.cpp
+++ calc/main.cpp
@@ -2,7 +2,7 @@
 Younes Bakir
 Calculator
 9/9/25
-Extra: TBD
+Extra: Changing Color Text
 */
 
 #include <iostream>
@@ -19,6 +19,14 @@ HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 for (int i = 0; i < 16; i++) {
 SetConsoleTextAttribute(hConsole, i);
 cout << "Enter a number: ";
+cin >> num1;
+cout << "Enter a second number: ";
+cin >> num2;
+cout << "Added: " << num1 + num2 << endl;
+cout << "Subtracted: " << num1 - num2 << endl;
+cout << "Multiplied: " << num1 * num2 << endl;
+cout << "Divided: " << num1 / num2 << endl;
+cout << "Modulo: " << num1 % num2 << endl;
 std::this_thread::sleep_for(std::chrono::milliseconds(1));
 }
 
